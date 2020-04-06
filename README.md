cafe1e842b334e020617219eb1e6844109e50d4d
jumi sanchez <jumary.sanchez@codeimmersives.com>
2020-04-05 22:06:55 -0400

    read.me

--- README.md
+++ README.md
@@ -1,43 +1,39 @@
-# Todata - The Director's Cut
+# Todata
 
-There are three main ways we could extend Todata.
+### Goal
+Create a To-do list that works
 
-1. Give it a good README.
-2. Upgrade its functionality.
-3. Make it beautiful.
+### What Does That Entail?
 
+Ive started over from scratch using DOM manipulation to create a fully functional to-do app
 
-### README
+No NPM test needed
 
-This is our absolute first priority with this project. This is what you will be presenting FIRST in your presentations on Monday.
+### HTML 
 
-The reason is that it's time we started focusing on the polish of our apps. And as (currently) solo developers, your apps are first and foremost demonstrations of your ability.
+HTML is important to help build an outline for your app. We will start by giving names to certain div families. These class names will later be called upon when we use DOM manipulation. 
 
-Whatever the state of your projects, be they just started or ready to deploy, they will be much more attractive packages with full documentation. It shows that you think deeply about what you're doing and can explain it succinctly. And if you can't explain it succinctly... well, it's time to start practicing!
+What is DOM?
 
-Here's what your README should include:
-1. A screenshot of the app. (Feel free to update this if you change its look!
-2. The app's features from a user perspective.
-3. What tools and techniques you used.
-4. Which of those tools were new to you.
-5. Where the app is hosted.
-
-For hosting the app, use one of the following methods:
-* [GitHub Pages](https://pages.github.com/). Has the advantage that every time you push to GH, it will (with some delay) update the hosted page using your changes.
-* [Surge](https://surge.sh/). My personal favorite, as setup is easy and you can do it with one command from the command line.
-
-Some good resources on what makes a good README in larger projects (including the ones you'll build later this term):
-
-* An article about [what makes a good README for an open-source project](https://dev.to/scottydocs/how-to-write-a-kickass-readme-5af9#template). It also has a great README template.
-* This one is just [another good README template](https://gist.github.com/fvcproductions/1bfc2d4aecb01a834b46).
-* [README Driven Development](https://tom.preston-werner.com/2010/08/23/readme-driven-development.html) - A short manifesto for writing the README as the very first thing.
+## DOM
+What is DOM manipulation in JavaScript?
+The Document Object Model (DOM) represents that same document so it can be manipulated. The DOM is an object-oriented representation of the web page, which can be modified with a scripting language such as JavaScript.
 
 
-### Upgrade Its Functionality.
+## HOW DOES DOM WORK?
 
-Guidelines coming soon!
+It allows a language (JavaScript) to manipulate, structure, and style your website. After the browser reads your HTML document, it creates a representational tree called the Document Object Model and defines how that tree can be accessed.
+
+![](/img.png)
 
 
-### Make It Beautiful
+### Some Problems You'll Have To Solve
 
-Guidelines coming soon!
+* How can we toggle the completeness of a todo by clicking on it?
+* How can we change the text of the buttons that are toggling? For example, when we click "Show Priorities", it changes to "Hide Priorities" so that we can toggle that feature back off.
+* How can we access the pulldown menu for adding priority to a todo?
+
+
+### An Important Guideline:
+
+You should be using INLINE functions for your event listeners. No more having to make (and name!) helper functions.
--- document.js
+++ document.js
@@ -1,71 +1,21 @@
-const element = document.getElementById("element");
+// const element = document.getElementById("element");
 
 
-const clear = document.querySelector(".clear")
+// const clear = document.querySelector(".clear")
 
 
-// const getDate= document.getElementById("date")
+// // const getDate= document.getElementById("date")
 
-const input = document.getElementById("input")
-
-
-
-const list = document.getElementById("list")
-
-let LIST= [];
-let id= 0
-
-
-document.addEventListener("keyup",function(event){ 
-
-    if(event.keyCode == 13){
-        const toDo = input.value;
-    if(toDo){
-        addToDo(toDo,id,false,false)
-        LIST.push(
-            {
-                name: toDo,
-                id: id,
-                done: false,
-                trash:false,
-            })
-        } input.value = ""
-                id++
-    }
-    })
-
-const CHECK= document.querySelector(".thin-circle-completed")
-const UNCHECK= document.querySelector(".trash-deleted")
-const lineThrough= "lineThrough"
-
-function addToDo(toDo, ) {
-
-    if(trash){
-        return toDo;
-    }
-
-    const DONE = done ? CHECK : UNCHECK;
-    const line = done ? lineThrough: "";
-
-const text= `<li class = "item">
-                <i class = "thin-circle-completed ${DONE}" job= "complete" id="${id}" ></i>
-                <p class="text ${line}" >${toDo}</p>
-                <i class = "trash-deleted" job ="delete id="${id}" ></i>
-                <i class= "priority" ></i> 
-            </li>`
-            const position = "beforeEnd";
-            
-    list.insertAdjacentHTML(position, text)
-    
-    return addToDo()
-}
+// const input = document.getElementById("input")
 
 
 
+// const list = document.getElementById("list")
 
 // let LIST= [];
 // let id= 0
-// // const input = document.getElementById("input")
+
+
 // document.addEventListener("keyup",function(event){ 
 
 //     if(event.keyCode == 13){
@@ -84,30 +34,135 @@ const text= `<li class = "item">
 //     }
 //     })
 
+// const CHECK= document.querySelector(".thin-circle-completed")
+// const UNCHECK= document.querySelector(".trash-deleted")
+// const lineThrough= "lineThrough"
 
-list.addEventListener("click", function(event){
-    let element = event.target;
-    const elementJOB = event.target.attributes.job.value;    
-    if(elementJOB == "complete"){
-        completeTodo(element)
-    }else if(elementJOB == "delete"){
-        removeTodo(element)
-    }
-})
+// function addToDo(toDo, ) {
+
+//     if(trash){
+//         return toDo;
+//     }
+
+//     const DONE = done ? CHECK : UNCHECK;
+//     const line = done ? lineThrough: "";
+
+// const text= `<li class = "item">
+//                 <i class = "thin-circle-completed ${DONE}" job= "complete" id="${id}" ></i>
+//                 <p class="text ${line}" >${toDo}</p>
+//                 <i class = "trash-deleted" job ="delete id="${id}" ></i>
+//                 <i class= "priority" ></i> 
+//             </li>`
+//             const position = "beforeEnd";
+            
+//     list.insertAdjacentHTML(position, text)
+    
+//     return addToDo()
+// }
 
 
 
 
-//MOVED TO MAIN.JS  but did not work //
-function completeTodo(element){
-    element.classList.toggle(CHECK);
-    element.classList.toggle(UNCHECK);
-    element.parentNode.querySelector(".text").classList.toggle(lineThrough)
-    LIST[element.id].done = LIST[element.id].done ? false : true; 
+// // let LIST= [];
+// // let id= 0
+// // // const input = document.getElementById("input")
+// // document.addEventListener("keyup",function(event){ 
+
+// //     if(event.keyCode == 13){
+// //         const toDo = input.value;
+// //     if(toDo){
+// //         addToDo(toDo,id,false,false)
+// //         LIST.push(
+// //             {
+// //                 name: toDo,
+// //                 id: id,
+// //                 done: false,
+// //                 trash:false,
+// //             })
+// //         } input.value = ""
+// //                 id++
+// //     }
+// //     })
+
+
+// list.addEventListener("click", function(event){
+//     let element = event.target;
+//     const elementJOB = event.target.attributes.job.value;    
+//     if(elementJOB == "complete"){
+//         completeTodo(element)
+//     }else if(elementJOB == "delete"){
+//         removeTodo(element)
+//     }
+// })
+
+
+
+
+// //MOVED TO MAIN.JS  but did not work //
+// function completeTodo(element){
+//     element.classList.toggle(CHECK);
+//     element.classList.toggle(UNCHECK);
+//     element.parentNode.querySelector(".text").classList.toggle(lineThrough)
+//     LIST[element.id].done = LIST[element.id].done ? false : true; 
+// }
+
+
+// function removeTodo(element){
+//     element.parentNode.parentNode.removeChild(element.parentNode);
+//     LIST[element.id].trash = true;
+// }
+
+let myNodelist = document.getElementsByTagName("LI");
+let i;
+for (i = 0; i < myNodelist.length; i++) {
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
 }
 
+// Click on a close button to hide the current list item
+let close = document.getElementsByClassName("close");
+let i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+    }
+}
 
-function removeTodo(element){
-    element.parentNode.parentNode.removeChild(element.parentNode);
-    LIST[element.id].trash = true;
+// Add a "checked" symbol when clicking on a list item
+let list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+    }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("myInput").value;
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+    alert("You must write something!");
+    } else {
+    document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        let div = this.parentElement;
+        div.style.display = "none";
+    }
+    }
 }
